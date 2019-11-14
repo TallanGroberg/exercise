52a392bf4dfe891e145d7b6d0fa5a148e7cb6532
tallan groberg <tallan.taven@gmail.com>
2019-11-14 17:33:13 -0600

    init

---
+++ nov15.js
@@ -0,0 +1,33 @@
+// const lessThanOrEqual = (num) => {
+//   if(num > 0) {
+//     return false
+//   } else if (num === 0) {
+//     return true
+//   } else {
+//     return true
+//   }
+// }
+
+// console.log(lessThanOrEqual(0))
+
+// const squared = (a) => a * a 
+
+// console.log(squared(5))
+
+// const nameShuffle = str => str.split(' ').reverse().join(' ')
+
+// console.log(nameShuffle('tallan groberg'))
+
+// const isIdentical = str => {
+//    const toArr = str.split('')
+//    const toSet = new Set(toArr)
+//    const backToArr = [...toSet]
+//    return backToArr.length === 1 ? true : false
+// }
+
+
+// const isIdentical = s => new Set(s).size === 1
+
+// const isIdentical = s => [...s].every(lett => lett === s[0])
+
+// console.log(isIdentical('ssssd'))
